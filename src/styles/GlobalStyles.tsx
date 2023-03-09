ff6ca646c18389a86dd73fef2ac3a6535134dbb4
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-09 19:35:43 +0100

    Add GlobalStyles

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+import "../../node_modules/@fontsource/merriweather/400.css";
+import "../../node_modules/@fontsource/nunito-sans/400.css";
+
+const GlobalStyles = createGlobalStyle`
+*, 
+::before,
+::after {
+  box-sizing: border-box
+}
+
+body {
+  margin: 0px;
+  font-family: ${(props) => props.theme.fonts.text};
+}
+
+ul, 
+ol {
+  list-style: none;
+  padding: 0;
+}
+
+a,
+a:active,
+a:visited {
+  color: inherit;
+  text-decoration: none;
+}
+
+button {
+  font-family: inherit;
+  font-size: inherit;
+  background-color: ${(props) => props.theme.palette.gray};
+  border-radius: ${(props) => props.theme.inputs.borderRadiusLarge};
+  border: none;
+  cursor: pointer;
+}
+
+.cointainer {
+  margin: 20px 
+}`;
+
+export default GlobalStyles;
