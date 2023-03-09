0e917ede4b392bca9e91f649b55f0e38e8d80e4d
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-09 18:54:08 +0100

    Add style-components theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,26 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  palette: {
+    primary: "#303030",
+    secondary: "#242424",
+    white: "#fff",
+    gray: "f5f5f5",
+    success: "#55b938",
+    error: "#d65745",
+  },
+  fonts: {
+    title: "Merriweather",
+    titleFontSizeNormal: "1.25rem",
+    titleFontSizeMid: "1.5rem",
+    titleFontSizeBig: "2.25rem",
+    text: "Nunito Sans",
+    textFontSizeNormal: "1rem",
+    textFontSizeMid: "1.125rem",
+    textFontSizeBig: "1.875rem",
+  },
+  inputs: {
+    borderRadiusSmall: "4px",
+    borderRadiusLarge: "8px",
+  },
+};
