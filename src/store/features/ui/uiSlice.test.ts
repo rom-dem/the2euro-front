fd2ed7ea4e79f46ca8a3ab70a82481478bbd52ec
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-10 20:10:03 +0100

    Test uiSlice, setModal and unsetModal

---
+++ src/store/features/ui/uiSlice.test.ts
@@ -0,0 +1,52 @@
+import { ModalPayload, UiState } from "./types";
+import {
+  setModalActionCreator,
+  uiReducer,
+  unsetModalActionCreator,
+} from "./uiSlice";
+
+describe("Given a uiReducer reducer", () => {
+  describe("When called with setModal action with a message 'Something went wrong'", () => {
+    test("Then it should return a new state with property isError set to true and modal with the given message", () => {
+      const currentUiState: UiState = {
+        isError: false,
+        modal: "",
+      };
+      const modalMessage = "Something went wrong";
+      const modalPayload: ModalPayload = {
+        isError: true,
+        modal: modalMessage,
+      };
+
+      const expectedState: UiState = {
+        ...currentUiState,
+        isError: true,
+        modal: modalMessage,
+      };
+      const setModal = setModalActionCreator(modalPayload);
+      const newUiState = uiReducer(currentUiState, setModal);
+
+      expect(newUiState).toStrictEqual(expectedState);
+    });
+  });
+
+  describe("When called with unsetModal action", () => {
+    test("Then it should return a new state with property isError set to false and modal with an empty string", () => {
+      const currentUiState: UiState = {
+        isError: true,
+        modal: "Something went wrong",
+      };
+      const modalMessage = "";
+      const expectedState: UiState = {
+        ...currentUiState,
+        isError: false,
+        modal: modalMessage,
+      };
+
+      const unsetModal = unsetModalActionCreator();
+      const newUiState = uiReducer(currentUiState, unsetModal);
+
+      expect(newUiState).toStrictEqual(expectedState);
+    });
+  });
+});
