aa1a0309196ae01f5cc533fa435a2a5c978d93cb
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-11 14:32:19 +0100

    Add error and success modals

---
+++ src/modals/modals.ts
@@ -0,0 +1,29 @@
+import { toast } from "react-toastify";
+
+export const showErrorModal = (message: string) => {
+  toast.error(message, {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    closeButton: false,
+  });
+};
+
+export const showSuccessModal = (message: string) => {
+  toast.success(message, {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    closeButton: false,
+  });
+};
