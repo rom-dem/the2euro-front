3cac6dbc08b34b5726ec331f71094ac3f4f4e6e0
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-11 21:55:01 +0100

    Add router to routers

---
+++ src/routers/router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouteObject } from "react-router-dom";
+import App from "../App";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import endpoints from "./endpoints";
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <App />,
+    children: [{ path: endpoints.login, element: <LoginPage /> }],
+  },
+];
+
+export const router = createBrowserRouter(routes);
