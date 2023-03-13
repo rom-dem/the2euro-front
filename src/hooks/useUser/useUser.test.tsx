56e5ad7b0f0fa39a6144c5e0480f0350ec21b562
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-13 23:11:37 +0100

    Add testing to useUser error case

--- src/hooks/useUser/useUser.test.tsx
+++ src/hooks/useUser/useUser.test.tsx
@@ -6,8 +6,10 @@ import { User } from "../../store/features/users/types";
 import { loginUserActionCreator } from "../../store/features/users/userSlice";
 import { CustomTokenPayload, UserCredentials } from "./types";
 import { server } from "../../mocks/server";
-import { handlers } from "../../mocks/handlers";
+import { errorHandlers, handlers } from "../../mocks/handlers";
 import useUser from "./useUser";
+import { ModalPayload } from "../../store/features/ui/types";
+import { setModalActionCreator } from "../../store/features/ui/uiSlice";
 
 beforeAll(() => {
   jest.clearAllMocks();
@@ -29,22 +31,23 @@ describe("Given the useUser custom hook", () => {
 
   const mockTocken = "1234asdf";
 
+  const {
+    result: {
+      current: { loginUser },
+    },
+  } = renderHook(() => useUser(), { wrapper: Wrapper });
+
   describe("When its loginUser function is called with user credentials", () => {
     beforeEach(() => {
       server.resetHandlers(...handlers);
     });
+
     test("Then it should call dispatch function with the user credentials", async () => {
       const mockLoggedinUser: User = {
         email: mockTockenPayload.email,
         id: mockTockenPayload.id,
         token: mockTocken,
       };
-
-      const {
-        result: {
-          current: { loginUser },
-        },
-      } = renderHook(() => useUser(), { wrapper: Wrapper });
 
       (jwtDecode as jest.MockedFunction<typeof jwtDecode>).mockReturnValue(
         mockTockenPayload
@@ -54,6 +57,29 @@ describe("Given the useUser custom hook", () => {
 
       expect(dispatchSpy).toHaveBeenCalledWith(
         loginUserActionCreator(mockLoggedinUser)
+      );
+    });
+  });
+
+  describe("When its loginUser function is called and fails", () => {
+    beforeEach(() => {
+      server.resetHandlers(...errorHandlers);
+    });
+
+    test("Then it should call the dispatch method with setModalActionCreator with the message 'Wrong credentials'", async () => {
+      const errorMessage = "Wrong credentials";
+
+      const modal: ModalPayload = {
+        isError: true,
+        isSuccess: false,
+        message: errorMessage,
+      };
+
+      await loginUser(mockUserCredentials);
+
+      expect(dispatchSpy).toHaveBeenNthCalledWith(
+        3,
+        setModalActionCreator(modal)
       );
     });
   });
