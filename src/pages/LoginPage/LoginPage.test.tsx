001c47c3cc82533b9c3e0eef58d59f8c8de0045b
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-14 15:17:21 +0100

    Add testing to LoginPage

--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -1,6 +1,13 @@
 import { screen } from "@testing-library/react";
+import * as ReactRouterDom from "react-router-dom";
+import { UserState } from "../../store/features/users/types";
 import { renderRouterWithProviders } from "../../testUtil/renderWithProviders";
 import LoginPage from "./LoginPage";
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  Navigate: jest.fn(),
+}));
 
 describe("Given a LoginPage page", () => {
   describe("When rendered", () => {
@@ -11,6 +18,20 @@ describe("Given a LoginPage page", () => {
       const button = screen.getByRole("button", { name: buttonText });
 
       expect(button).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user is logged in", () => {
+    test("Then it should call 'Navigate'", () => {
+      const user: UserState = {
+        email: "",
+        id: "",
+        token: "",
+        isLogged: true,
+      };
+
+      renderRouterWithProviders(<LoginPage />, { user: user });
+      expect(ReactRouterDom.Navigate).toHaveBeenCalled();
     });
   });
 });
