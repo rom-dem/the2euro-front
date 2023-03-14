c66763c4be87ee7700a55b010423f7c4f7a9654b
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-14 16:08:24 +0100

    Join pages styles in StyledPage and use them

--- src/pages/MyCoinsPage/MyCoinsPageStyled.tsx
+++ src/pages/MyCoinsPage/MyCoinsPageStyled.tsx
@@ -1,27 +1,6 @@
 import styled from "styled-components";
+import StyledPage from "../../styles/StyledPage/StyledPage";
 
-const MyCoinsPageStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-  font-family: ${(props) => props.theme.fonts.text};
-  min-height: 100vh;
-  max-width: 100%;
-  justify-content: space-between;
-  position: relative;
-
-  .page {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    &__title {
-      font-family: ${(props) => props.theme.fonts.title};
-      font-size: ${(props) => props.theme.fonts.titleFontSizeS};
-      padding-top: 50px;
-      display: table-cell;
-      vertical-align: bottom;
-      align-self: center;
-    }
-  }
-`;
+const MyCoinsPageStyled = styled(StyledPage)``;
 
 export default MyCoinsPageStyled;
