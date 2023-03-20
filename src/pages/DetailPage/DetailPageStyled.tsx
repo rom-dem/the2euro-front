af466cf542b58279c1d8ab7d33a5110acccb1016
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-20 10:34:21 +0100

    Add styles to DetailPage page

---
+++ src/pages/DetailPage/DetailPageStyled.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import StyledPage from "../../styles/StyledPage/StyledPage";
+
+const DetailPageStyled = styled(StyledPage)``;
+
+export default DetailPageStyled;
