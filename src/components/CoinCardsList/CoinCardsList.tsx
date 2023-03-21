3ddc2cdd9cc240745067c860b2c95bc5e78f71ab
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-21 14:35:52 +0100

    Delete loadAllCoin function in CoinCardsList

--- src/components/CoinCardsList/CoinCardsList.tsx
+++ src/components/CoinCardsList/CoinCardsList.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-import useApi from "../../hooks/useApi/useApi";
 import { useAppSelector } from "../../store/hooks";
 import CoinCard from "../CoinCard/CoinCard";
 import CoinsListStyled from "./CoinCardsListStyled";
@@ -7,12 +5,6 @@ import { ReactComponent as EditIcon } from "../../assets/icons/edit.svg";
 import { ReactComponent as DeleteIcon } from "../../assets/icons/delete.svg";
 
 const CoinCardsList = (): JSX.Element => {
-  const { loadAllCoins } = useApi();
-
-  useEffect(() => {
-    loadAllCoins();
-  }, [loadAllCoins]);
-
   const coins = useAppSelector((state) => state.coins.coins);
   return (
     <CoinsListStyled>
