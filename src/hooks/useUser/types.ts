1d0b53c9ac8dbcaf7b73a9d0f57400ec347ebe5b
Róman Demchuk <roman.demchuk@gmail.com>
2023-04-04 17:40:17 +0200

    Add registerUser to useUser custom hook

--- src/hooks/useUser/types.ts
+++ src/hooks/useUser/types.ts
@@ -3,9 +3,16 @@ export interface UserCredentials {
   email: string;
 }
 
+export interface UserRegisterCredentials extends UserCredentials {
+  username: string;
+}
+
 export interface UseUserStructure {
   loginUser: (userCredentials: UserCredentials) => Promise<void>;
   logoutUser: () => void;
+  registerUser: (
+    userRegisterCredentials: UserRegisterCredentials
+  ) => Promise<void>;
 }
 
 export interface LoginResponse {
