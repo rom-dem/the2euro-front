3baf4e47ba2f0cff4b7121008a75e89378adb4fd
Róman Demchuk <roman.demchuk@gmail.com>
2023-04-05 17:53:33 +0200

    Add RegisterFormStyled component

---
+++ src/components/RegisterForm/RegisterFormStyled.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+const RegisterFormStyled = styled.form`
+  font-family: ${(props) => props.theme.fonts.text};
+  color: #303030;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  select {
+    margin-left: -4px;
+  }
+
+  .form {
+    &__pill {
+      background-color: ${(props) => props.theme.colour.gray};
+      padding: 8px 16px;
+      border-radius: ${(props) => props.theme.border.borderRadiusSmall};
+    }
+
+    &__label {
+      display: flex;
+      flex-direction: column;
+      gap: 5px;
+      min-height: 22px;
+      font-size: ${(props) => props.theme.fonts.textFontSizeS};
+    }
+
+    &__input {
+      min-height: 25px;
+      font-size: ${(props) => props.theme.fonts.textFontSizeM};
+    }
+  }
+`;
+
+export default RegisterFormStyled;
